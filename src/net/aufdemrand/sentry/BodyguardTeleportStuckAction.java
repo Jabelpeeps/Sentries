3e41e83476e778cda194b2342d13a55f2fbfded7
Jabelpeeps <Jabelpeeps@users.noreply.github.com>
2015-08-24 00:22:57 +0100

    Update BodyguardTeleportStuckAction.java
    reformatting for clarity
    changing Runnable in final section from annonymous to a finalised, named object to avoid potential memory leaks.

--- src/net/aufdemrand/sentry/BodyguardTeleportStuckAction.java
+++ src/net/aufdemrand/sentry/BodyguardTeleportStuckAction.java
@@ -8,10 +8,13 @@ import net.citizensnpcs.api.ai.Navigator;
 import net.citizensnpcs.api.ai.StuckAction;
 import net.citizensnpcs.api.npc.NPC;
 
-public class BodyguardTeleportStuckAction implements StuckAction{
-	SentryInstance inst =null;
+public class BodyguardTeleportStuckAction implements StuckAction {
+	SentryInstance inst = null;
 	Sentry plugin = null;
-	BodyguardTeleportStuckAction(SentryInstance inst, Sentry plugin){
+	
+	private static int MAX_ITERATIONS = 10;
+	
+	BodyguardTeleportStuckAction( SentryInstance inst, Sentry plugin ){
 		this.inst = inst; 
 		this.plugin = plugin;
 	}
@@ -19,42 +22,40 @@ public class BodyguardTeleportStuckAction implements StuckAction{
 	@Override
 	public boolean run(final NPC npc, Navigator navigator) {
 
-		if (!npc.isSpawned()) return false;
+		if ( !npc.isSpawned() ) return false;
 		
 		Location base = navigator.getTargetAsLocation();
 
-		if (base.getWorld() == npc.getEntity().getLocation().getWorld()){
-			if (npc.getEntity().getLocation().distanceSquared(base) <= 4)
-				//do nothing
+		if ( base.getWorld() == npc.getEntity().getLocation().getWorld() 
+		  && npc.getEntity().getLocation().distanceSquared( base ) <= 4 )
+				// do nothing, as already at target.
 				return true;
-		}
-		else {
-			//do nothing, next logic tick will clear the entity.
-			if (inst.guardEntity ==null || !Util.CanWarp(inst.guardEntity, npc)) return true; 
-		}
+	        else if ( inst.guardEntity == null 
+		       || !Util.CanWarp( inst.guardEntity, npc ) ) 
+			        // do nothing, next logic tick will clear the entity.
+			        return true; 
 			
-
 		Block block = base.getBlock();
-		int iterations = 0;
-		while (!block.isEmpty()) {
-			block = block.getRelative(BlockFace.UP);
-			if (++iterations >= MAX_ITERATIONS && !block.isEmpty())
+		
+		int i = 0;
+		while ( !block.isEmpty() ) {
+			block = block.getRelative( BlockFace.UP );
+			if ( ++i >= MAX_ITERATIONS && !block.isEmpty() )
 				block = base.getBlock();
 			break;
 		}
 
 		final Location loc = block.getLocation();
+		
+		final Runnable tpEvent = new Runnable(){
 
-		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
-
-			@Override
-			public void run() {
-				npc.teleport(loc, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);	
+			@Override public void run() {
+				npc.teleport( loc, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN );	
 			}
+		}
 
-		},2);
+		plugin.getServer().getScheduler().scheduleSyncDelayedTask( plugin, tpEvent, 2 );
 
 		return false;
 	}
-	private static int MAX_ITERATIONS = 10;
 }
