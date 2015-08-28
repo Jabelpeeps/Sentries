d31045812ab1ee7ba171793914d23e447f94385e
Jabelpeeps <Jabelpeeps@users.noreply.github.com>
2015-08-28 12:31:31 +0100

    Update SpiderAttackStrategy.java
    adding spacing and comments

--- src/net/aufdemrand/sentry/SpiderAttackStrategy.java
+++ src/net/aufdemrand/sentry/SpiderAttackStrategy.java
@@ -7,33 +7,33 @@ import net.minecraft.server.v1_8_R3.Entity;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.LivingEntity;
 
-public class SpiderAttackStrategy implements net.citizensnpcs.api.ai.AttackStrategy{
+public class SpiderAttackStrategy implements net.citizensnpcs.api.ai.AttackStrategy {
+	
 	private Random random = new Random();
-
 	Sentry plugin = null;
 
-	public SpiderAttackStrategy(Sentry plugin) {
+	public SpiderAttackStrategy( Sentry plugin ) {
 		this.plugin = plugin;
 	}
 
 	@Override
-	public boolean handle(LivingEntity arg0, LivingEntity arg1) {
+	public boolean handle( LivingEntity arg0, LivingEntity arg1 ) {
 
-		plugin.debug("Spider ATTACK!");
+		plugin.debug( "Spider ATTACK!" );
 
-		Entity entity = ((CraftEntity)arg1).getHandle();
-		Entity me = ((CraftEntity)arg0).getHandle();
+		Entity entity = ( (CraftEntity) arg1 ).getHandle();
+		Entity me = ( (CraftEntity) arg0 ).getHandle();
 
-		if (this.random.nextInt(20) == 0) {
+		if ( this.random.nextInt( 20 ) == 0 ) {
 				double d0 = entity.locX - me.locX;
 				double d1 = entity.locZ - me.locZ;
-				double f2 = Math.sqrt(d0 * d0 + d1 * d1);
-
+				double f2 = Math.sqrt( d0 * d0 + d1 * d1 );
+			// TODO consider pulling out the double values as constants
+			// (at first glance they look like multiple of the same number in any case.)
 				me.motX = d0 /  f2 * 0.5D * 0.800000011920929D + me.motX * 0.20000000298023224D;
 				me.motZ = d1 /  f2 * 0.5D * 0.800000011920929D + me.motZ * 0.20000000298023224D;
 				me.motY = 0.4000000059604645D;
 		}
-
 		return false;
 	}
 }
