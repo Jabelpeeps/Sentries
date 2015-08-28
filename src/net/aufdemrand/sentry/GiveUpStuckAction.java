4178daed0a5d410a4b4ede735d895042514786d5
Jabelpeeps <Jabelpeeps@users.noreply.github.com>
2015-08-28 18:56:01 +0100

    Update GiveUpStuckAction.java
    reformatting if statement to use && notation.

--- src/net/aufdemrand/sentry/GiveUpStuckAction.java
+++ src/net/aufdemrand/sentry/GiveUpStuckAction.java
@@ -6,29 +6,27 @@ import net.citizensnpcs.api.ai.Navigator;
 import net.citizensnpcs.api.ai.StuckAction;
 import net.citizensnpcs.api.npc.NPC;
 
-public class GiveUpStuckAction implements StuckAction{
-	SentryInstance inst =null;
+public class GiveUpStuckAction implements StuckAction {
 	
-	GiveUpStuckAction(SentryInstance inst){
+	SentryInstance inst = null;
+	
+	GiveUpStuckAction( SentryInstance inst ){
 		this.inst = inst; 
 	}
 
 	@Override
-	public boolean run(NPC npc, Navigator navigator) {
-	//	inst.plugin.getServer().broadcastMessage("give up stuck action");
-		if (!npc.isSpawned())		
-			return false;
-        Location base = navigator.getTargetAsLocation();
+	public boolean run( NPC npc, Navigator navigator ) {
+		// inst.plugin.getServer().broadcastMessage("give up stuck action");
+		
+		if ( !npc.isSpawned() ) return false;
+		
+        	Location base = navigator.getTargetAsLocation();
         
-        if (base.getWorld() == npc.getEntity().getLocation().getWorld()){
-            if (npc.getEntity().getLocation().distanceSquared(base) <= 4)
-                return true;
-        }
-
-		inst.setTarget(null, false);
+        	if ( base.getWorld() == npc.getEntity().getLocation().getWorld() 
+        	  && npc.getEntity().getLocation().distanceSquared( base ) <= 4 ) {
+                	return true;
+        	}	
+		inst.setTarget( null, false );
 		return false;
 	}
-
-
-
 }
